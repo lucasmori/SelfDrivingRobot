01e0602d6ad8b87af25c1a69d3f77314cc54ebcf
lucasmori <lucastecnew@gmail.com>
2019-11-07 21:49:19 -0300

    Adiciona front

--- frontend/src/pages/Homepage/index.js
+++ frontend/src/pages/Homepage/index.js
@@ -1,28 +1,37 @@
-import React, { useState } from 'react';
-import api from '../../services/api';
+import React from "react";
+import api from "../../services/api";
+import "./styles.css";
 
 export default function Homepage() {
-
   async function startSubmit() {
     console.log("iniciou");
-    const response = await api.get('/run', {});
+    const response = await api.get("/run", {});
     console.log(response.data);
   }
 
   async function stopSubmit() {
     console.log("parou");
-    const response = await api.get('/stop', {});
+    const response = await api.get("/stop", {});
     console.log(response.data);
   }
 
   return (
-    <>
-      <div className="form">
-        <h1>Self Driving Robot</h1>
-        <label>Control Movement</label>
-        <button className="btn start" onClick={startSubmit}>Start</button>
-        <button className="btn" onClick={stopSubmit}>Stop</button>
+    <div className="centraliza">
+      <div className="container-pai">
+        <div className="centraliza-header">
+          <div className="header">
+            <h2>CONTROLE DE MOVIMENTO</h2>
+          </div>
         </div>
-    </>
-  )
+        <div className="container-botoes">
+          <button className="iniciarButton" onClick={startSubmit}>
+            INICIAR
+          </button>
+          <button className="pararButton" onClick={stopSubmit}>
+            PARAR
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
